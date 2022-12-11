a890f288ea280e522a48aee214ef90db9cecee34
ferneyh57 <ferneyh57@gmail.com>
2022-12-10 19:33:26 -0500

    clean router

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import { Request, Response } from "express";
 import { BaseProduct, Product } from "../interface/product.interface";
 import * as ProductService from "../service/product.service"
 
--- src/router/product.router.ts
+++ src/router/product.router.ts
@@ -1,36 +1,11 @@
-/**
- * Required External Modules and Interfaces
- */
-
 import express from "express";
 import * as controller from "../controller/product.controller";
 
-/**
-* Router Definition
-*/
-
 export const productsRouter = express.Router();
 
-/**
- * Controller Definitions
- */
-
-// GET products
-
-productsRouter.get("/", controller.getAllProducts);
-
-// GET products/:id
-
-productsRouter.get("/:id", controller.getProductById);
-
-// POST products
-
-productsRouter.post("/", controller.addProduct);
-
-// PUT products/:id
-
-productsRouter.put("/:id", controller.updateProduct);
-
-// DELETE products/:id
-
-productsRouter.delete("/:id", controller.deleteProduct);
+productsRouter
+    .get("/", controller.getAllProducts)
+    .get("/:id", controller.getProductById)
+    .post("/", controller.addProduct)
+    .put("/:id", controller.updateProduct)
+    .delete("/:id", controller.deleteProduct);
