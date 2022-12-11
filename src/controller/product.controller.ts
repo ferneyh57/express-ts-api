044922f64b45988f4704ead0f6716d60af3e8b79
ferneyh57 <ferneyh57@gmail.com>
2022-12-10 19:24:42 -0500

    add controller

---
+++ src/controller/product.controller.ts
@@ -0,0 +1,73 @@
+import express, { Request, Response } from "express";
+import { BaseProduct, Product } from "../interface/product.interface";
+import * as ProductService from "../service/product.service"
+
+export const getAllProducts = async (req: Request, res: Response) => {
+    try {
+        const products: Product[] = await ProductService.findAll();
+
+        res.status(200).send(products);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
+
+export const getProductById = async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id, 10);
+
+    try {
+        const findProduct: Product = await ProductService.find(id);
+
+        if (findProduct) {
+            return res.status(200).send(findProduct);
+        }
+
+        res.status(404).send("Product not found");
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
+
+export const addProduct =  async (req: Request, res: Response) => {
+    try {
+        const product: BaseProduct = req.body;
+
+        const newProduct = await ProductService.create(product);
+
+        res.status(201).json(newProduct);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
+
+export const updateProduct =  async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id, 10);
+
+    try {
+        const newProductData: Product = req.body;
+
+        const findProduct: Product = await ProductService.find(id);
+
+        if (findProduct) {
+            const updatedProduct = await ProductService.update(id, newProductData);
+            return res.status(200).json(updatedProduct);
+        }
+
+        const newProduct = await ProductService.create(newProductData);
+
+        res.status(201).json(newProduct);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    try {
+        const productId: number = parseInt(req.params.id, 10);
+        await ProductService.remove(productId);
+
+        res.sendStatus(204);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
--- src/router/product.router.ts
+++ src/router/product.router.ts
@@ -2,9 +2,9 @@
  * Required External Modules and Interfaces
  */
 
-import express, { Request, Response } from "express";
-import { BaseProduct, Product } from "../interface/product.interface";
-import * as ProductService from "../service/product.controller"
+import express from "express";
+import * as controller from "../controller/product.controller";
+
 /**
 * Router Definition
 */
@@ -17,80 +17,20 @@ export const productsRouter = express.Router();
 
 // GET products
 
-productsRouter.get("/", async (req: Request, res: Response) => {
-    try {
-        const products: Product[] = await ProductService.findAll();
-
-        res.status(200).send(products);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
+productsRouter.get("/", controller.getAllProducts);
 
 // GET products/:id
 
-productsRouter.get("/:id", async (req: Request, res: Response) => {
-    const id: number = parseInt(req.params.id, 10);
-
-    try {
-        const findProduct: Product = await ProductService.find(id);
-
-        if (findProduct) {
-            return res.status(200).send(findProduct);
-        }
-
-        res.status(404).send("Product not found");
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
+productsRouter.get("/:id", controller.getProductById);
 
 // POST products
 
-productsRouter.post("/", async (req: Request, res: Response) => {
-    try {
-        const product: BaseProduct = req.body;
-
-        const newProduct = await ProductService.create(product);
-
-        res.status(201).json(newProduct);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
+productsRouter.post("/", controller.addProduct);
 
 // PUT products/:id
 
-productsRouter.put("/:id", async (req: Request, res: Response) => {
-    const id: number = parseInt(req.params.id, 10);
-
-    try {
-        const newProductData: Product = req.body;
-
-        const findProduct: Product = await ProductService.find(id);
-
-        if (findProduct) {
-            const updatedProduct = await ProductService.update(id, newProductData);
-            return res.status(200).json(updatedProduct);
-        }
-
-        const newProduct = await ProductService.create(newProductData);
-
-        res.status(201).json(newProduct);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
+productsRouter.put("/:id", controller.updateProduct);
 
 // DELETE products/:id
 
-productsRouter.delete("/:id", async (req: Request, res: Response) => {
-    try {
-        const productId: number = parseInt(req.params.id, 10);
-        await ProductService.remove(productId);
-
-        res.sendStatus(204);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
+productsRouter.delete("/:id", controller.deleteProduct);
